92664bcd920548f5e6e9384a2ca1cd9c76c2ab87
Sahanmi <2sahanmi3@gmail.com>
2026-08-20 18:29:10 +0530

    Add passing test for appointment save

--- src/main/java/com/sunrisedental/dao/AppointmentDAO.java
+++ src/main/java/com/sunrisedental/dao/AppointmentDAO.java
@@ -2,6 +2,8 @@ package com.sunrisedental.dao;
 
 import com.sunrisedental.model.Appointment;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class AppointmentDAO {
@@ -9,6 +11,26 @@ public class AppointmentDAO {
     public boolean saveAppointment(Appointment appointment)
             throws SQLException {
 
-        return false;
+        Connection connection =
+                DBConnectionFactory.getConnection();
+
+        PreparedStatement statement =
+                connection.prepareStatement(
+                        "INSERT INTO appointments " +
+                                "(appointment_id, appointment_number) " +
+                                "VALUES (?, ?)"
+                );
+
+        statement.setInt(
+                1,
+                appointment.getAppointmentId()
+        );
+
+        statement.setString(
+                2,
+                appointment.getAppointmentNumber()
+        );
+
+        return statement.executeUpdate() > 0;
     }
 }
