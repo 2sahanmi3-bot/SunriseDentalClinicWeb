1dd969e3763d1822890f870a9d3f231dfea509bd
Sahanmi <2sahanmi3@gmail.com>
2026-08-20 17:55:14 +0530

    Add AppointmentDAO to save appointments

---
+++ src/main/java/com/sunrisedental/dao/AppointmentDAO.java
@@ -0,0 +1,14 @@
+package com.sunrisedental.dao;
+
+import com.sunrisedental.model.Appointment;
+
+import java.sql.SQLException;
+
+public class AppointmentDAO {
+
+    public boolean saveAppointment(Appointment appointment)
+            throws SQLException {
+
+        return false;
+    }
+}
