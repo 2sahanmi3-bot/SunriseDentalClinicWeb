dfe56187d4662150418eb963cbf22600c439faf6
Sahanmi <2sahanmi3@gmail.com>
2026-08-21 13:24:00 +0530

    Add failing test for appointment registration

--- src/test/java/com/sunrisedental/controller/AppointmentControllerTest.java
+++ src/test/java/com/sunrisedental/controller/AppointmentControllerTest.java
@@ -1,12 +1,16 @@
 package com.sunrisedental.controller;
 
+import com.sunrisedental.model.Appointment;
 import com.sunrisedental.service.AppointmentService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.*;
 
 class AppointmentControllerTest {
 
@@ -32,5 +36,39 @@ class AppointmentControllerTest {
 
         response =
                 mock(HttpServletResponse.class);
+    }
+
+    @Test
+    void postRegisterShouldRegisterAppointmentAndRedirect()
+            throws Exception {
+
+        when(request.getParameter("action"))
+                .thenReturn("register");
+
+        when(request.getParameter("appointmentNumber"))
+                .thenReturn("APT001");
+
+        when(appointmentService.registerAppointment(
+                any(Appointment.class)))
+                .thenReturn(true);
+
+        controller.doPost(
+                request,
+                response
+        );
+
+        verify(appointmentService)
+                .registerAppointment(
+                        argThat(appointment ->
+                                "APT001".equals(
+                                        appointment.getAppointmentNumber()
+                                )
+                        )
+                );
+
+        verify(response)
+                .sendRedirect(
+                        "appointment?action=search&appointmentNumber=APT001"
+                );
     }
 }
