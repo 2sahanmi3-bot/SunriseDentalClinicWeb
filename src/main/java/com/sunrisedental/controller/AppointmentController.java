1535b3241ebbbd48f1faa7696676185d0c2c5cf4
Sahanmi <2sahanmi3@gmail.com>
2026-08-21 13:16:18 +0530

    add AppointmentController

---
+++ src/main/java/com/sunrisedental/controller/AppointmentController.java
@@ -0,0 +1,29 @@
+package com.sunrisedental.controller;
+
+import com.sunrisedental.service.AppointmentService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class AppointmentController extends HttpServlet {
+
+    private AppointmentService appointmentService;
+
+    public AppointmentController(
+            AppointmentService appointmentService) {
+
+        this.appointmentService = appointmentService;
+    }
+
+    @Override
+    protected void doPost(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
