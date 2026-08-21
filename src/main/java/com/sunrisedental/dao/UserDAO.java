ceb39dad66e5f63bfb688c2f6bf55408af2f8fa1
Sahanmi <2sahanmi3@gmail.com>
2026-08-22 00:45:01 +0530

    Add passing test for staff user search

--- src/main/java/com/sunrisedental/dao/UserDAO.java
+++ src/main/java/com/sunrisedental/dao/UserDAO.java
@@ -4,11 +4,44 @@ import com.sunrisedental.model.User;
 
 import java.sql.SQLException;
 import java.util.Optional;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class UserDAO {
 
     public Optional<User> findByUsername(String username)
             throws SQLException {
+
+        Connection connection =
+                DBConnectionFactory.getConnection();
+
+        PreparedStatement statement =
+                connection.prepareStatement(
+                        "SELECT user_id, username, password " +
+                                "FROM users " +
+                                "WHERE username = ?"
+                );
+
+        statement.setString(
+                1,
+                username
+        );
+
+        ResultSet resultSet =
+                statement.executeQuery();
+
+        if (resultSet.next()) {
+
+            User user =
+                    new User(
+                            resultSet.getInt("user_id"),
+                            resultSet.getString("username"),
+                            resultSet.getString("password")
+                    );
+
+            return Optional.of(user);
+        }
 
         return Optional.empty();
     }
