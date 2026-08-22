3d5d592369905f814e620582f655f5668ae2e062
Sahanmi <2sahanmi3@gmail.com>
2026-08-22 20:21:17 +0530

    Add BillingService for bill calculation

---
+++ src/main/java/com/sunrisedental/service/BillingService.java
@@ -0,0 +1,11 @@
+package com.sunrisedental.service;
+
+public class BillingService {
+
+    public double calculateTotal(
+            double treatmentCharge,
+            double consultationFee) {
+
+        return 0.0;
+    }
+}
