825452f162d7cf2230ef21410fd3edd90b012824
Sahanmi <2sahanmi3@gmail.com>
2026-08-22 20:23:03 +0530

    Add BillingService test class

---
+++ src/test/java/com/sunrisedental/service/BillingServiceTest.java
@@ -0,0 +1,15 @@
+package com.sunrisedental.service;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class BillingServiceTest {
+
+    private BillingService billingService;
+
+    @BeforeEach
+    void setUp() {
+        billingService =
+                new BillingService();
+    }
+}
+
