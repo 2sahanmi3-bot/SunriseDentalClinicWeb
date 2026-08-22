977997d5ba1d9e575cecfb24b57917c0ef1c8f9b
Sahanmi <2sahanmi3@gmail.com>
2026-08-22 20:32:07 +0530

    Add passing test for bill total calculation

--- src/main/java/com/sunrisedental/service/BillingService.java
+++ src/main/java/com/sunrisedental/service/BillingService.java
@@ -6,6 +6,7 @@ public class BillingService {
             double treatmentCharge,
             double consultationFee) {
 
-        return 0.0;
+        return treatmentCharge
+                + consultationFee;
     }
 }
