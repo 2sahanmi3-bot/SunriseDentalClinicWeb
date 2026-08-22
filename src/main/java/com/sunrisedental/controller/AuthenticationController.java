6d5a110021ba8f01ff1bc601634e976a335020e4
Sahanmi <2sahanmi3@gmail.com>
2026-08-22 12:05:24 +0530

    Add passing test for staff login success

--- src/main/java/com/sunrisedental/controller/AuthenticationController.java
+++ src/main/java/com/sunrisedental/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import com.sunrisedental.service.AuthenticationService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -27,5 +28,37 @@ public class AuthenticationController extends HttpServlet {
             HttpServletResponse response)
             throws ServletException, IOException {
 
+        String username =
+                request.getParameter("username");
+
+        String password =
+                request.getParameter("password");
+
+        try {
+
+            boolean authenticated =
+                    authenticationService.authenticate(
+                            username,
+                            password
+                    );
+
+            if (authenticated) {
+
+                HttpSession session =
+                        request.getSession();
+
+                session.setAttribute(
+                        "staffUser",
+                        username
+                );
+
+                response.sendRedirect(
+                        "appointment"
+                );
+            }
+
+        } catch (SQLException e) {
+            throw new ServletException(e);
+        }
     }
 }
