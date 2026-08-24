078ffb2701fdde5aeed3fbe47d4fdb64adee921d
Sahanmi <2sahanmi3@gmail.com>
2026-08-25 02:26:59 +0530

    Add HelpController

---
+++ src/main/java/com/sunrisedental/controller/HelpController.java
@@ -0,0 +1,19 @@
+package com.sunrisedental.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelpController extends HttpServlet {
+
+    @Override
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
