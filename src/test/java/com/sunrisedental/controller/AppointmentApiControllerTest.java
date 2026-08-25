c173e05e9418b3e9eb8d4756e00832c2c13ec011
Sahanmi <2sahanmi3@gmail.com>
2026-08-25 08:10:56 +0530

    Add appointment web service test class

---
+++ src/test/java/com/sunrisedental/controller/AppointmentApiControllerTest.java
@@ -0,0 +1,35 @@
+package com.sunrisedental.controller;
+
+import com.sunrisedental.service.AppointmentService;
+import org.junit.jupiter.api.BeforeEach;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.mockito.Mockito.mock;
+
+class AppointmentApiControllerTest {
+
+    private AppointmentService appointmentService;
+    private AppointmentApiController controller;
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+
+    @BeforeEach
+    void setUp() {
+
+        appointmentService =
+                mock(AppointmentService.class);
+
+        controller =
+                new AppointmentApiController(
+                        appointmentService
+                );
+
+        request =
+                mock(HttpServletRequest.class);
+
+        response =
+                mock(HttpServletResponse.class);
+    }
+}
