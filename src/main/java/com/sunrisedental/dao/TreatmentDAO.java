809087a6294f628b15c442854d533c40502aaa8b
Sahanmi <2sahanmi3@gmail.com>
2026-09-01 02:15:06 +0530

    Pass treatment lookup test

--- src/main/java/com/sunrisedental/dao/TreatmentDAO.java
+++ src/main/java/com/sunrisedental/dao/TreatmentDAO.java
@@ -2,6 +2,9 @@ package com.sunrisedental.dao;
 
 import com.sunrisedental.model.Treatment;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
 
@@ -11,8 +14,69 @@ public class TreatmentDAO {
             String treatmentName)
             throws SQLException {
 
-        throw new UnsupportedOperationException(
-                "Not implemented"
-        );
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+
+            connection =
+                    DBConnectionFactory.getConnection();
+
+            statement =
+                    connection.prepareStatement(
+                            "SELECT treatment_id, treatment_name, " +
+                                    "treatment_charge, consultation_fee " +
+                                    "FROM treatments " +
+                                    "WHERE treatment_name = ?"
+                    );
+
+            statement.setString(
+                    1,
+                    treatmentName
+            );
+
+            resultSet =
+                    statement.executeQuery();
+
+            if (resultSet.next()) {
+
+                Treatment treatment =
+                        new Treatment(
+                                resultSet.getInt(
+                                        "treatment_id"
+                                ),
+                                resultSet.getString(
+                                        "treatment_name"
+                                ),
+                                resultSet.getDouble(
+                                        "treatment_charge"
+                                ),
+                                resultSet.getDouble(
+                                        "consultation_fee"
+                                )
+                        );
+
+                return Optional.of(
+                        treatment
+                );
+            }
+
+            return Optional.empty();
+
+        } finally {
+
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (statement != null) {
+                statement.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 }
