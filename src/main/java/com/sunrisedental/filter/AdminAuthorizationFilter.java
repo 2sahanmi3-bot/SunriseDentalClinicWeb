9ffec3b281740c8a1c169adb8b56736c924b8921
Sahanmi <2sahanmi3@gmail.com>
2026-09-04 04:06:35 +0530

    Restrict admin routes by user role

--- src/main/java/com/sunrisedental/filter/AdminAuthorizationFilter.java
+++ src/main/java/com/sunrisedental/filter/AdminAuthorizationFilter.java
@@ -7,6 +7,10 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 @WebFilter("/admin/*")
@@ -19,8 +23,46 @@ public class AdminAuthorizationFilter implements Filter {
             FilterChain chain)
             throws IOException, ServletException {
 
-        throw new UnsupportedOperationException(
-                "Not implemented"
+        HttpServletRequest httpRequest =
+                (HttpServletRequest) request;
+
+        HttpServletResponse httpResponse =
+                (HttpServletResponse) response;
+
+        HttpSession session =
+                httpRequest.getSession(false);
+
+        // Send the user back to login if there is no active staff session.
+        if (session == null
+                || session.getAttribute("staffUser") == null) {
+
+            httpResponse.sendRedirect(
+                    httpRequest.getContextPath()
+                            + "/login.jsp"
+            );
+
+            return;
+        }
+
+        String role =
+                (String) session.getAttribute(
+                        "staffRole"
+                );
+
+        // Staff users should not be able to open admin pages.
+        if (!"ADMIN".equals(role)) {
+
+            httpResponse.sendError(
+                    HttpServletResponse.SC_FORBIDDEN
+            );
+
+            return;
+        }
+
+        chain.doFilter(
+                request,
+                response
         );
     }
 }
+
